c53455c2e9d2d260ead2716baa902081781cbe0d
Chris <cmmooney852@gmail.com>
2022-03-15 16:35:22 +1000

    improve positioning, add map as resource

--- src/main.rs
+++ src/main.rs
@@ -10,54 +10,64 @@ fn main() {
             ..Default::default()
         })
         .add_plugins(DefaultPlugins)
+        .init_resource::<Map>()
         .add_startup_system(setup)
         .add_system(player_movement)
+        .add_system(draw_map)
         .run()
 }
 
 #[derive(Component)]
-struct Player {
+struct Player {}
+
+#[derive(Component)]
+struct Position {
     x: f32,
     y: f32,
 }
 
-#[derive(PartialEq, Clone, Copy)]
+#[derive(PartialEq, Clone, Copy, Debug)]
 enum TileType {
     Wall,
     Floor,
 }
 
+#[derive(Default)]
+struct Map {
+    tiles: Vec<TileType>,
+}
+
 fn player_movement(
     keyboard_input: Res<Input<KeyCode>>,
-    mut query: Query<(&mut Player, &mut Transform)>,
+    mut query: Query<(&mut Player, &mut Position, &mut Transform)>,
 ) {
     const MOVE_DISTANCE: f32 = 16.0;
     const SPRITE_BUFFER: f32 = 8.0;
     const X_BOUNDS: f32 = 320.0 - SPRITE_BUFFER;
     const Y_BOUNDS: f32 = 240.0 - SPRITE_BUFFER;
 
-    for (mut player, mut trans) in query.iter_mut() {
+    for (mut _player, mut position, mut trans) in query.iter_mut() {
         if keyboard_input.just_pressed(KeyCode::Right) {
-            player.x += MOVE_DISTANCE;
+            position.x += MOVE_DISTANCE;
         }
         if keyboard_input.just_pressed(KeyCode::Left) {
-            player.x -= MOVE_DISTANCE;
+            position.x -= MOVE_DISTANCE;
         }
         if keyboard_input.just_pressed(KeyCode::Up) {
-            player.y += MOVE_DISTANCE;
+            position.y += MOVE_DISTANCE;
         }
         if keyboard_input.just_pressed(KeyCode::Down) {
-            player.y -= MOVE_DISTANCE;
+            position.y -= MOVE_DISTANCE;
         }
 
         // Apply movement deltas
-        trans.translation.x += player.x;
+        trans.translation.x += position.x;
         trans.translation.x = trans.translation.x.clamp(-X_BOUNDS, X_BOUNDS);
-        trans.translation.y += player.y;
+        trans.translation.y += position.y;
         trans.translation.y = trans.translation.y.clamp(-Y_BOUNDS, Y_BOUNDS);
 
-        player.x = 0.0;
-        player.y = 0.0;
+        position.x = 0.0;
+        position.y = 0.0;
     }
 }
 
@@ -65,28 +75,83 @@ fn new_map() -> Vec<TileType> {
     let mut map = vec![TileType::Floor; 40 * 30];
 
     // Make the edges walls
-    for x in (0..640).step_by(16) {
+    for x in 0..40 {
         map[map_idx(x, 0)] = TileType::Wall;
-        map[map_idx(x, 39)] = TileType::Wall;
+        map[map_idx(x, 29)] = TileType::Wall;
     }
 
-    for y in (0..640).step_by(16) {
+    for y in 0..30 {
         map[map_idx(0, y)] = TileType::Wall;
-        map[map_idx(29, y)] = TileType::Wall;
+        map[map_idx(39, y)] = TileType::Wall;
     }
 
     map
+}
+
+fn draw_map(
+    map: Res<Map>,
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+) {
+    let mut y = 0.0;
+    let mut x = 0.0;
+
+    // Setup the sprite sheet
+    let texture_handle = asset_server.load("spritesheet.png");
+    let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(16.0, 16.0), 16, 16);
+    let texture_atlas_handle = texture_atlases.add(texture_atlas);
+
+    for tile in map.tiles.iter() {
+        // Render a tile depending upon the tile type
+        match tile {
+            TileType::Floor => {
+                // Spawn the floor
+                commands
+                    .spawn()
+                    .insert_bundle(SpriteSheetBundle {
+                        texture_atlas: texture_atlas_handle.clone(),
+                        transform: Transform::from_translation(Vec3::new(0.0, -220.0, 0.0)),
+                        sprite: TextureAtlasSprite::new(sprite_idx(0, 2)),
+                        ..Default::default()
+                    })
+                    .insert(Position { x, y });
+            }
+            TileType::Wall => {
+                // Spawn the Wall
+                commands
+                    .spawn()
+                    .insert_bundle(SpriteSheetBundle {
+                        texture_atlas: texture_atlas_handle.clone(),
+                        transform: Transform::from_translation(Vec3::new(0.0, -220.0, 0.0)),
+                        sprite: TextureAtlasSprite::new(sprite_idx(0, 1)),
+                        ..Default::default()
+                    })
+                    .insert(Position { x, y });
+            }
+        }
+
+        // Move the coordinates
+        x += 16.0;
+        if x > 640.0 {
+            x = 0.0;
+            y += 16.0;
+        }
+    }
 }
 
 fn setup(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
     mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+    mut map: ResMut<Map>,
 ) {
     // Setup the sprite sheet
     let texture_handle = asset_server.load("spritesheet.png");
     let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(16.0, 16.0), 16, 16);
     let texture_atlas_handle = texture_atlases.add(texture_atlas);
+
+    map.tiles = new_map();
 
     // Add a 2D Camera
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
@@ -100,7 +165,8 @@ fn setup(
             sprite: TextureAtlasSprite::new(sprite_idx(4, 8)),
             ..Default::default()
         })
-        .insert(Player { x: 0.0, y: 0.0 });
+        .insert(Player {})
+        .insert(Position { x: 10.0, y: 10.0 });
 }
 
 pub fn sprite_idx(x: i32, y: i32) -> usize {
